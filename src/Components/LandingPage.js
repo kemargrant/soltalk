0f0224c4fe062e97252aaaf2ba548f59fc01d336
sol-survivor <sol-survivor@sol-talk.com>
2022-01-08 16:56:03 -0500

    update landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,39 +1,45 @@
 import React from 'react';
 
-function LandingPage(props){ 	
-	return(
-		<div className="landingBody">
-			<div id="landingLogoDiv">
-				<img id="landingLogo" src="/images/landing_logo.png" alt="sol survivor logo"/>
-			</div>
-			<div id="landingMidBody">
-				<p id="landingMainMessage">
-					FIGHTING EVOLVED
-					<br/>
-					<br/>
-					
-					<a href="https://sol-survivor.gitbook.io/docs/" target="_blank" rel="noopener noreferrer"><button className="landingButton">Read The Docs</button></a>
-					<button className="landingButton" onClick={props.goToApp}>Launch APP</button>
-					<a href="https://nft.solsurvivor.net/" target="_blank" rel="noopener noreferrer"><button className="landingButton">NFTs</button></a>
-					
-					<br/>
-					<br/>
-				</p>	
-			</div>
-			<div id="landingLowerBody">
-				<a href="https://twitter.com/sol__survivor?ref_src=twsrc%5Etfw" target="_blank" rel="noopener noreferrer"> 
-					<img src="/images/twitter.png" alt="twitter logo"/>								
-				</a>
-				<a href="https://github.com/kemargrant/soltalk" target="_blank" aria-label="Star kemargrant/soltalk on GitHub" rel="noopener noreferrer">
-					<img src="/images/github_light.png" alt="github logo"/>								
-				</a>	
-				<a href="https://solana.com" target="_blank" rel="noopener noreferrer">
-					<img src="/images/built_for_solana.svg" alt="solana logo"/>								
-				</a>
-				<a href="https://www.unlimitedcope.com/" target="_blank" rel="noopener noreferrer"> Powered by <img alt="cope logo" src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/8HGyAAB1yoM1ttS7pXjHMa3dukTFGQggnFFH3hJZgzQh/logo.png" id="copeLogo"/> </a>
-			</div>
-		</div>
-		)
-	}
+let isMobile = false;
+if(window.innerHeight > window.innerWidth){ isMobile = true; }
 
+function LandingPage(props){ 	
+	return(<div id="Landing_body">
+			<div id="Landing_site">
+				{	
+					!isMobile ?	
+					<div id="Landing_movie">
+						<video autoplay="" loop id="video1"> 
+							<source src="https://nft.solsurvivor.net/demofight.mp4" type="video/webm" /> 
+						</video>
+					</div>
+					:null
+				}
+				<div id="Landing_cards">
+					<div className="Landing_card" >
+						<b onClick={props.goToApp}> APP </b>
+					</div>
+					<div className="Landing_card" >
+						<a href="https://sol-survivor.gitbook.io/docs"> TUTORIAL </a>
+					</div>				
+					<div className="Landing_card" >
+						<a href="https://nft.solsurvivor.net"> NFTs </a>
+					</div>				
+				</div>
+			</div>
+			<div className="row" id="Landing_support"> 
+				<a href="https://twitter.com/sol__survivor" target="_blank" rel="noopener noreferrer">
+					<img src="https://solsurvivor.net/images/twitter.png" alt="twitter logo" class="img-fluid"/>
+				</a>            
+
+				<a href="https://github.com/kemargrant/soltalk" target="_blank" aria-label="Star kemargrant/soltalk on GitHub" rel="noopener noreferrer">
+					<img src="https://solsurvivor.net/images/github_light.png" alt="github logo" class="img-fluid"/>
+				</a>
+
+				<a href="https://solana.com" target="_blank" rel="noopener noreferrer">
+					<img src="https://solsurvivor.net/images/built_for_solana.svg" alt="solana logo" class="img-fluid" />
+				</a>
+			</div>
+		</div>)
+	}
 export { LandingPage }
